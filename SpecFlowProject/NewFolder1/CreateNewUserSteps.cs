a98d10886506c212458ecd4109097d38f54dcee2
Annette Curnow <acurnow@panviva.com>
2015-08-07 17:11:29 +1000

    New features

---
+++ SpecFlowProject/NewFolder1/CreateNewUserSteps.cs
@@ -0,0 +1,64 @@
+﻿using SP_Automation.Tests;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject.NewFolder1
+{
+    [Binding]
+    public class CreateNewUserSteps
+    {
+        [Given(@"I have logged into SP, as username (.*) and password (.*)")]
+        public void GivenIHaveLoggedIntoSPAsUsernamePanvivaAndPasswordBurke(string username, string password)
+        {
+            SupportPoint.OpenSupportPoint();
+            SupportPoint.LogIn.Login(username, password);
+        }
+
+       
+        
+        [Given(@"I am at User Management page")]
+        public void GivenIAmAtUserManagementPage()
+        {
+            SupportPoint.Nav.ToSupportPointManager();
+            //  
+            SupportPoint.SwitchToSPManager();
+            System.Threading.Thread.Sleep(5000);
+            SupportPoint.SPManagerNav.ClickUsers();
+        }
+        
+        [When(@"I press Action")]
+        public void WhenIPressAction()
+        {
+            SupportPoint.User.ClickAction();
+            
+        }
+        
+        [When(@"I press New User")]
+        public void WhenIPressNewUser()
+        {
+            SupportPoint.User.ClickNewUser();
+        }
+        
+        [When(@"I have entered (.*), (.*), (.*), (.*), (.*), (.*) into the username, firstname, lastname, email, password, verify password")]
+        public void WhenIHaveEnteredAdvAuthorAdvAuthorTestPanviva_ComPasswordPasswordIntoTheUsernameFirstnameLastnameEmailPasswordVerifyPassword(string name, string firstname, string lastname, string email, string password, string verifypassword)
+        {
+            SupportPoint.User.CreateNewUser(name, firstname, lastname, email, password, verifypassword);
+            ScenarioContext.Current.Add("username", name);
+        }
+        
+        [When(@"I press Save")]
+        public void WhenIPressSave()
+        {
+            SupportPoint.User.Save();
+        }
+        
+       
+        
+        [Then(@"the result should be new user added")]
+        public void ThenTheResultShouldBeNewUserAdded()
+        {
+            string username = ScenarioContext.Current.Get<string>("username");
+           // SupportPoint.User.VerifyUserExist();
+        }
+    }
+}
